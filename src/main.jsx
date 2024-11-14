9264e31b56c4ec6a318bd9d772f4a5b594f74be4
sucidprtt <pratiwisucidwii@gmail.com>
2024-11-14 18:36:38 +0700

    final: 7.7 State pada React

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
 
-var count = 0;
-
-function increase() {
-  count = count + 1
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <div>
-        <h1>{count}</h1>
-        <button onClick={increase}>+</button>
-      </div>
-    </React.StrictMode>,
-  )
-  
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
